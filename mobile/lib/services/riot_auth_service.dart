28fc9ca40ce3107d3f6193c5d0ef10b39835459d
Quang Hung <hungsct1702@gmail.com>
2026-07-23 22:38:06 +0700

    fix: chunk PUUID list into batches of 30 for Riot Name Service API requests

--- mobile/lib/services/riot_auth_service.dart
+++ mobile/lib/services/riot_auth_service.dart
@@ -437,22 +437,12 @@ class RiotAuthService {
 
         final Map<String, Map<String, String>> resolvedNames = {};
         if (puuidSet.isNotEmpty) {
-          try {
-            final url = Uri.parse('https://pd.$shard.a.pvp.net/name-service/v2/players');
-            var nameRes = await http.put(
-              url,
-              headers: {
-                'Authorization': 'Bearer $accessToken',
-                'X-Riot-Entitlements-JWT': entitlementToken,
-                'X-Riot-ClientVersion': clientVersion,
-                'X-Riot-ClientPlatform': clientPlatform,
-                'Content-Type': 'application/json',
-              },
-              body: jsonEncode(puuidSet.toList()),
-            );
-
-            if (nameRes.statusCode != 200) {
-              nameRes = await http.post(
+          final List<String> allPuuids = puuidSet.toList();
+          for (int i = 0; i < allPuuids.length; i += 30) {
+            final chunk = allPuuids.sublist(i, (i + 30 > allPuuids.length) ? allPuuids.length : i + 30);
+            try {
+              final url = Uri.parse('https://pd.$shard.a.pvp.net/name-service/v2/players');
+              var nameRes = await http.put(
                 url,
                 headers: {
                   'Authorization': 'Bearer $accessToken',
@@ -461,37 +451,51 @@ class RiotAuthService {
                   'X-Riot-ClientPlatform': clientPlatform,
                   'Content-Type': 'application/json',
                 },
-                body: jsonEncode(puuidSet.toList()),
+                body: jsonEncode(chunk),
               );
-            }
 
-            if (nameRes.statusCode == 200) {
-              final List nameList = jsonDecode(nameRes.body);
-              for (var item in nameList) {
-                final sub = (item['Subject'] ?? item['subject'] ?? item['puuid'] ?? '').toString();
-                String gName = (item['GameName'] ?? item['gameName'] ?? '').toString();
-                String tLine = (item['TagLine'] ?? item['tagLine'] ?? '').toString();
+              if (nameRes.statusCode != 200) {
+                nameRes = await http.post(
+                  url,
+                  headers: {
+                    'Authorization': 'Bearer $accessToken',
+                    'X-Riot-Entitlements-JWT': entitlementToken,
+                    'X-Riot-ClientVersion': clientVersion,
+                    'X-Riot-ClientPlatform': clientPlatform,
+                    'Content-Type': 'application/json',
+                  },
+                  body: jsonEncode(chunk),
+                );
+              }
 
-                if (gName.isEmpty && item['DisplayName'] != null) {
-                  final dn = item['DisplayName'].toString();
-                  if (dn.contains('#')) {
-                    final parts = dn.split('#');
-                    gName = parts[0];
-                    tLine = parts.sublist(1).join('#');
-                  } else {
-                    gName = dn;
+              if (nameRes.statusCode == 200) {
+                final List nameList = jsonDecode(nameRes.body);
+                for (var item in nameList) {
+                  final sub = (item['Subject'] ?? item['subject'] ?? item['puuid'] ?? '').toString();
+                  String gName = (item['GameName'] ?? item['gameName'] ?? '').toString();
+                  String tLine = (item['TagLine'] ?? item['tagLine'] ?? '').toString();
+
+                  if (gName.isEmpty && item['DisplayName'] != null) {
+                    final dn = item['DisplayName'].toString();
+                    if (dn.contains('#')) {
+                      final parts = dn.split('#');
+                      gName = parts[0];
+                      tLine = parts.sublist(1).join('#');
+                    } else {
+                      gName = dn;
+                    }
+                  }
+
+                  if (sub.isNotEmpty && gName.isNotEmpty) {
+                    resolvedNames[sub] = {
+                      'gameName': gName,
+                      'tagLine': tLine,
+                    };
                   }
                 }
-
-                if (sub.isNotEmpty && gName.isNotEmpty) {
-                  resolvedNames[sub] = {
-                    'gameName': gName,
-                    'tagLine': tLine,
-                  };
-                }
               }
-            }
-          } catch (_) {}
+            } catch (_) {}
+          }
         }
 
         for (var matchRes in responses) {
